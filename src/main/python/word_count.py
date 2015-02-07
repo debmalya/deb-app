0033560b349584ab196dc4e8f850f441b2d65131
debmalya <debmalya.jash@gmail.com>
2015-02-07 22:29:47 +0800

    Create word_count.py

---
+++ src/main/python/word_count.py
@@ -0,0 +1,16 @@
+name = raw_input('Enter file:')
+handle = open(name,'r')
+text = handle.read()
+words=text.split()
+counts=dict()
+for word in words:
+	counts[word] = counts.get(word,0)+1
+
+bigCount = None
+bigWord = None
+for word,count in counts.items():
+	if bigCount is None or count > bigCount:
+		bigWord = word
+		bigCount = count
+
+print bigWord,bigCount
